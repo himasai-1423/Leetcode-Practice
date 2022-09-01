869166cd984246640283ce9f59480c0dfdb97063
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-09-01 21:26:56 +0530

    Time: 14 ms (26.04%), Space: 10.1 MB (59.19%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> m;
-        vector<int> res;
-        for(int i=0; i<nums1.size(); i++){
-            m[nums1[i]]++;
-        }
-        
-        for(int &i: nums2){
-            if(m[i]){
-                res.push_back(i);
-                m[i]--;
+    int searchItem(vector<int> &v, int t, vector<int> &ans){
+        int s = 0, m, e = v.size() - 1;
+        while (s <= e)
+        {
+            m = s + (e - s) / 2;
+            if (v[m] == t)
+            {
+                ans.push_back(v[m]);
+                v.erase(v.begin() + m);
+                break;
+            }
+            if (v[m] < t)
+            {
+                s = m + 1;
+            }
+            else
+            {
+                e = m - 1;
             }
         }
-        return res;
+        return 0;
+    }
+    vector<int> intersect(vector<int> &n1, vector<int> &n2)
+    {
+        vector<int> ans;
+        sort(n1.begin(), n1.end());
+        for (auto i : n2)
+        {
+            searchItem(n1, i, ans);
+        }
+        return ans;
     }
 };
