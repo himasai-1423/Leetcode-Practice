faf7a47b0803d9b642631178bae0b498067c9894
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-09-01 21:38:38 +0530

    Time: 8 ms (95.29%), Space: 6 MB (11.77%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
-        int rev = 0;
-        while (rev < x) {
-            rev = rev * 10 + x % 10;
-            x /= 10;
-        }
-        
-        return x == rev || x == rev / 10;
+        string rev = to_string(x);
+        reverse(rev.begin(), rev.end());
+        return to_string(x) == rev;
     }
 };
