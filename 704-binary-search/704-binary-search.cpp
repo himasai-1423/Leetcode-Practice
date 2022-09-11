daa094664b08c334cc02bc170cb0724b8843d7c0
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-09-11 16:43:49 +0530

    Time: 95 ms (5.78%), Space: 27.7 MB (12.36%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int hi = nums.size()-1;
+        int lo = 0, mid;
+        
+        while(hi>=lo){
+            mid = lo + (hi-lo)/2;
+            if(nums[mid]== target) return mid;
+            
+            if(nums[mid]> target) hi = mid-1;
+            else lo = mid + 1;
+        }
+        // if(nums[lo] == target) return lo;
+        return -1;
+    }
+};
