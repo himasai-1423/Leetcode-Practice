a60b1a7ed4f97d0defe37779fbc5cfd9381998a5
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-09-11 15:47:12 +0530

    Time: 1 ms (43.36%), Space: 6 MB (23.09%) - LeetHub

---
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
@@ -0,0 +1,25 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int hi=n, lo = 1,mid, val ;
+        
+        while(hi>=lo){
+            mid=lo+(hi-lo)/2;
+            val=guess(mid);
+            
+            if(val==0) return mid;
+            else if(val==1) lo=mid+1;
+            else hi = mid-1;
+        }
+        return mid;
+    }
+};
