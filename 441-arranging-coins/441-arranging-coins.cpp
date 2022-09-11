a5d7324769437a6bcf3fa4405593d28975b2408b
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-09-11 16:23:56 +0530

    Time: 10 ms (44.59%), Space: 5.8 MB (69.28%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        long hi = n, lo =0, mid;
-        long coinsReq;
+        int hi = n, lo =0;
+        long mid, coinsReq;
         while(hi>=lo){
             mid = lo+(hi-lo)/2;
             coinsReq = mid*(mid+1)/2;
             
-            if(coinsReq == n) return (int)mid;
+            if(coinsReq == n) return mid;
             
             if(coinsReq>n) hi = mid-1;
             else lo = mid+1;
         }
-        return (int)hi;
+        return hi;
     }
 };
