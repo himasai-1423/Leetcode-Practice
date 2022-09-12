6a96d504bf770b31b5189e921c290124cec9e238
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-09-12 22:50:18 +0530

    Time: 9 ms (78.31%), Space: 13.7 MB (17.94%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans(2, -1);
-        int start = 0, end = (int)nums.size() - 1;
-        while(start <= end) {
-            int mid = start + (end-start)/2;
-            if(nums[mid] < target) start = mid+1;
-            else {
-                if(nums[mid] == target) ans[0] = mid;
-                end = mid-1;
+        int n = nums.size();
+        int hi = n-1, lo =0, mid;
+        vector<int> a(2,-1);
+        while(hi>=lo){
+            mid = lo + (hi-lo)/2;
+            if(nums[mid]>target) hi = mid - 1;
+            else if(nums[mid]<target) lo = mid +1;
+            else{
+                if(mid==lo || nums[mid] != nums[mid-1]){
+                    a[0] = mid;
+                    break;
+                } 
+                else{
+                    a[0]= mid-1;
+                    hi = mid-1;
+                }
             }
         }
-        start = 0, end = (int)nums.size() - 1;
-        while(start <= end) {
-            int mid = start + (end-start)/2;
-            if(nums[mid] > target) end = mid-1;
-            else {
-                if(nums[mid] == target) ans[1] = mid;
-                start = mid+1;
+        
+        hi = n-1, lo =0;
+        while(hi>=lo){
+            mid = lo + (hi-lo)/2;
+            if(nums[mid]>target) hi = mid - 1;
+            else if(nums[mid]<target) lo = mid +1;
+            else{
+                if(mid==hi || nums[mid] != nums[mid+1]){
+                    a[1] = mid;
+                    break;
+                }
+                else{
+                    a[1]=mid+1;
+                    lo=mid+1;
+                }
             }
         }
-        return ans;
+        return a;
     }
 };
