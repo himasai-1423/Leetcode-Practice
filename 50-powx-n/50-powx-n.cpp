ec3952fb626d316aca7fe85eeda5123309059266
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-09-16 23:21:33 +0530

    Time: 4 ms (34.51%), Space: 5.9 MB (46.18%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double val = pow(x, n);
-        return val;
+        double val;
+        if(n==0) return 1;
+        
+        double temp = myPow(x, n/2);
+        
+        if(n%2==0) return temp * temp;
+        else{
+            if(n>0) return x*temp*temp;
+            
+            else return (temp*temp)/x;
+        }
     }
 };
