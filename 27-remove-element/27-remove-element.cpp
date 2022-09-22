c76af19431a74bb205c11b5852e42201c008db02
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-09-22 11:55:53 +0530

    Time: 4 ms (47.50%), Space: 8.7 MB (92.03%) - LeetHub

---
+++ 27-remove-element/27-remove-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i=0, j=0, cnt=0;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            if(nums[i] != val){
+                nums[j] = nums[i];
+                j++;
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
