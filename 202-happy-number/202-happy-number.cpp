6d708a077f59c2f0cc2438fc5dddcdb0ee92fb4f
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-09-26 21:34:19 +0530

    Time: 4 ms (46.72%), Space: 5.9 MB (84.74%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-    int next(int n){
+    int next(int n)
+    {
         int sum = 0;
         
-        while(n != 0){
+        while(n != 0)
+        {
             sum += pow(n % 10,2);
             n = n / 10;
         }
+        
         return sum;
     }
 
 public:
     bool isHappy(int n) {
-        int slow = next(n);
-        int fast = next(next(n));
+        int slow = n;
+        int fast = next(n);
+        int cnt = 1;
+        int lim = 2;
         
-        while(slow != fast){
-            slow = next(slow);
-            fast = next(next(fast));
+        while(slow != fast)
+        {
+            if(cnt == lim)
+            {
+                cnt = 1;
+                lim = lim*2;
+                slow = fast;
+            }
+            else
+                cnt ++;
+        
+            fast = next(fast);
         }
         
         return fast == 1 ;
