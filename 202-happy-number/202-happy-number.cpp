849e858c880c459f84ef1da498da9fce2c011ad7
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-09-26 21:34:09 +0530

    Time: 0 ms (100.00%), Space: 6 MB (63.08%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    bool isHappy(int n) {
-        unordered_set<int> s;
-        while(n!=1){
-            if(s.find(n)==s.end()) s.insert(n);
-            else return false;
-            
-            int rem=0, sum=0;
-            while(n){
-                rem = n%10;
-                sum += (rem*rem);
-                n /= 10;
-            }
-            n = sum;
+    int next(int n){
+        int sum = 0;
+        
+        while(n != 0){
+            sum += pow(n % 10,2);
+            n = n / 10;
         }
-        return true;
+        return sum;
+    }
+
+public:
+    bool isHappy(int n) {
+        int slow = next(n);
+        int fast = next(next(n));
+        
+        while(slow != fast){
+            slow = next(slow);
+            fast = next(next(fast));
+        }
+        
+        return fast == 1 ;
     }
 };
