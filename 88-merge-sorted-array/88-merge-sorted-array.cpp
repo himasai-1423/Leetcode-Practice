9baaf9327d200c56fb2a245c81e36b7423b8898a
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-09-26 21:52:14 +0530

    Time: 12 ms (6.76%), Space: 9.1 MB (30.46%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -4,21 +4,10 @@ public:
         int i = m+n-1, a=m-1, b=n-1;
         
         while(a>=0 && b>=0){
-            if(nums1[a]>nums2[b]){
-                nums1[i] = nums1[a];
-                a--;
-            }
-            else {
-                nums1[i] = nums2[b];
-                b--;
-            }
-            i--;
+            if(nums1[a]>nums2[b]) nums1[i--] = nums1[a--];
+            else nums1[i--] = nums2[b--];
         }
         
-        while(b>=0){
-            nums1[i] = nums2[b];
-            b--;
-            i--;
-        }
+        while(b>=0) nums1[i--] = nums2[b--];
     }
 };
