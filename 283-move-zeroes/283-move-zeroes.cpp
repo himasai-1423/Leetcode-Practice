0cc777ecaf9ed375956668df267ca515bc7333d4
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-09-29 22:54:08 +0530

    Time: 51 ms (35.35%), Space: 19.3 MB (57.57%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j=0, n=nums.size();
-        for(int i=0; i<n; i++)
-            if(nums[i]!=0) nums[j++] = nums[i];
-        
-        while(j<n) nums[j++] = 0;
+    for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.size(); cur++) {
+        if (nums[cur] != 0) {
+            swap(nums[lastNonZeroFoundAt++], nums[cur]);
+        }
     }
+}
 };
