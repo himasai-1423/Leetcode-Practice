d85c79ea2f788db3927f0622beff4afd57c4bd45
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-10-01 20:06:55 +0530

    Time: 12 ms (75.99%), Space: 5.8 MB (98.04%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
-    int romanToInt(string s) 
-   {
-    unordered_map<char, int> T = { { 'I' , 1 },
-                                   { 'V' , 5 },
-                                   { 'X' , 10 },
-                                   { 'L' , 50 },
-                                   { 'C' , 100 },
-                                   { 'D' , 500 },
-                                   { 'M' , 1000 } };
-                                   
-   int sum = T[s.back()];
-   for (int i = s.length() - 2; i >= 0; --i) 
-   {
-       if (T[s[i]] < T[s[i + 1]])
-       {
-           sum -= T[s[i]];
-       }
-       else
-       {
-           sum += T[s[i]];
-       }
-   }  
-   return sum;
-}
+    int romanToInt(string s) {
+        int sum = 0;
+        char previous = 0;
+        for (auto iterator = s.begin(); iterator != s.end(); ++iterator) {
+            switch(*iterator){
+            case 'I':
+                ++sum;
+                break;
+            case 'V':
+                sum += 5;
+                if (previous == 'I') sum -= 2;
+                break;
+            case 'X':
+                sum += 10;
+                if (previous == 'I') sum -= 2;
+                break;
+            case 'L':
+                sum += 50;
+                if (previous == 'X') sum -= 20;
+                break;
+            case 'C':
+                sum += 100;
+                if (previous == 'X') sum -= 20;
+                break;
+            case 'D':
+                sum += 500;
+                if (previous == 'C') sum -= 200;
+                break;
+            case 'M':
+                sum += 1000;
+                if (previous == 'C') sum -= 200;
+                break;
+            }
+            previous = *iterator;
+        }
+        return sum;
+    }
 };
