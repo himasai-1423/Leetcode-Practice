dff6a83b3c60674754de8f861f573349e9a3960f
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-10-03 20:12:25 +0530

    Time: 28 ms (36.67%), Space: 15.8 MB (86.36%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> m;
+        int cnt1=0, cnt2=0, el1, el2;
         vector<int> res;
-        
         for(int i=0; i<nums.size(); i++){
-            m[nums[i]]++;
+            if(el1 == nums[i]) cnt1++;
+            else if(el2 == nums[i]) cnt2++;
+            else if(cnt1==0){
+                el1 = nums[i];
+                cnt1++;
+            }
+            else if(cnt2==0){
+                el2 = nums[i];
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            } 
         }
         
-        for(auto &i: m){
-            if(i.second > nums.size()/3) 
-                res.push_back(i.first);
+        cnt1=0, cnt2=0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == el1) cnt1++;
+            if(nums[i] == el2) cnt2++;
         }
+
+        if(cnt1>nums.size()/3) res.push_back(el1);
+        if(cnt2>nums.size()/3) res.push_back(el2);
+        
         return res;
     }
+    
 };
