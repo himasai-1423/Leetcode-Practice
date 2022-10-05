d7e202d2b181562f030fe2bb5e9141e1b00a75ff
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-10-05 09:59:32 +0530

    Time: 276 ms (18.44%), Space: 67.8 MB (11.05%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,37 +1,18 @@
 class Solution {
-    struct value
-    {
-        int startLeft; 
-        int endRight;
-        int totalSum;
-        int maxSum;
-        value(int s, int e, int t, int m):startLeft(s), endRight(e), totalSum(t), maxSum(m){}
-    };
-    
-    value divideAndConquer(vector<int> const& nums, int l, int r)
-    {
-        if (l==r)
-        {
-            return {nums[l], nums[l],nums[l],nums[l]};
-        }
-        
-        int m = l + (r-l)/2;
-        
-        auto fromLeft=divideAndConquer(nums, l, m);
-        auto fromRight=divideAndConquer(nums, m+1, r);
-        
-        int s = max(fromLeft.startLeft, fromLeft.totalSum+fromRight.startLeft);
-        int e = max(fromRight.endRight, fromRight.totalSum+fromLeft.endRight);
-        int t = fromLeft.totalSum + fromRight.totalSum;
-        int ma = max(max(fromLeft.endRight+fromRight.startLeft, fromLeft.maxSum), fromRight.maxSum);
-        
-        return {s,e,t,ma};
-        
-    }
 public:
     int maxSubArray(vector<int>& nums) {
-        auto v = divideAndConquer(nums, 0, nums.size()-1);
+        int curr_maxi = INT_MIN;
+        int final_maxi = INT_MIN;
         
-        return v.maxSum;
+        for(int i=0; i<nums.size(); i++){
+            if(curr_maxi<0)
+                curr_maxi = nums[i];
+            
+            else
+                curr_maxi+=nums[i];
+            
+            final_maxi = max(final_maxi, curr_maxi);
+        }
+        return final_maxi;
     }
 };
