81ac1ea3e98b6a5e42109fef9096bf00d8bccd1b
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-10-05 12:12:18 +0530

    Time: 272 ms (19.38%), Space: 67.8 MB (11.05%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,26 +1,15 @@
-int init = [] {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    ofstream out("user.out");
-    cout.rdbuf(out.rdbuf());
-    for (string s; getline(cin, s); cout << '\n') {
-        int ans = INT_MIN, sum = 0;
-        for (int _i = 1, _n = s.length(); _i < _n; ++_i) {
-            bool _neg = false;
-            if (s[_i] == '-') ++_i, _neg = true;
-            int v = s[_i++] & 15;
-            while ((s[_i] & 15) < 10) v = v * 10 + (s[_i++] & 15);
-            if (_neg) v = -v;
-            sum = max(sum + v, v);
-            ans = max(ans, sum);
-        }
-        cout << ans;
-    }
-    exit(0);
-    return 0;
-}();
-
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {return 0;}
+    int maxSubArray(vector<int>& nums) {
+        int cur=0;
+        int maxi=INT_MIN;
+        for(auto i:nums)
+        {
+            cur+=i;
+            maxi = max(maxi , cur);
+            if(cur<0)
+                cur=0;
+        }
+        return maxi;
+    }
 };
