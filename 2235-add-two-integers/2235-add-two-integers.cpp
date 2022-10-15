63d91305f9fba8f144f36974d99c3604fcf3f5d6
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-10-15 20:02:08 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (72.95%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.cpp
+++ 2235-add-two-integers/2235-add-two-integers.cpp
@@ -1,6 +1,6 @@
-class Solution {
-public:
-    int sum(int num1, int num2) {
-        return num1+num2;
-    }
-};
+#define Actions class Solution {
+#define speak public: int sum(
+#define louder int num1, int num2) {
+#define than return num1 + num2; }
+#define words };
+Actions speak louder than words
