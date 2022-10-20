717b8c1240820b1f6eb7933cbef4a2b8e3642c5a
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-10-20 22:17:12 +0530

    Time: 3 ms (74.35%), Space: 9.1 MB (83.18%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -11,25 +11,15 @@
  */
 class Solution {
 public:
-    void calculateNumbers(TreeNode* root, vector<int> &val, int num){
-        if(!root->left && !root->right){
-            cout<<"Val: "<<num<<endl;
-            val.push_back(num);
-            return;
-        }
-        
-        if(root->left) calculateNumbers(root->left, val, num*10+root->left->val);
-        if(root->right) calculateNumbers(root->right, val, num*10+root->right->val);
-    }
-    
     int sumNumbers(TreeNode* root) {
-        vector<int> val;
-        calculateNumbers(root, val, root->val);
-        
-        int res;
-        for(int i=0; i<val.size(); i++){
-            res+=val[i];
-        }
-        return res;
+        return dfs(root, 0);
     }
+
+    int dfs(TreeNode* root, int cur) {
+        if(!root) return 0;
+        cur = cur * 10 + root -> val;
+        if(!root -> left && !root -> right)
+            return cur;
+        return dfs(root -> left, cur) + dfs(root -> right, cur);
+    }  
 };
