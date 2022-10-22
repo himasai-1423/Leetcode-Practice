d6b8ecdcae3c81407050656b1efb32aafe4f07a0
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-10-22 18:58:23 +0530

    Time: 286 ms (92.56%), Space: 144.6 MB (91.55%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -11,10 +11,22 @@
  */
 class Solution {
 public:
-    int minDepth(TreeNode *root) {
-        if(!root) return 0;
-        if(!root->left) return 1 + minDepth(root->right);
-        if(!root->right) return 1 + minDepth(root->left);
-        return 1+min(minDepth(root->left),minDepth(root->right));
+    int minDepth(TreeNode* root) {
+    if (!root) return 0;
+    queue<TreeNode*> Q;
+    Q.push(root);
+    int i = 0;
+    while (!Q.empty()) {
+        i++;
+        int k = Q.size();
+        for (int j=0; j<k; j++) {
+            TreeNode* rt = Q.front();
+            if (rt->left) Q.push(rt->left);
+            if (rt->right) Q.push(rt->right);
+            Q.pop();
+            if (rt->left==NULL && rt->right==NULL) return i;
+        }
     }
+    return -1; //For the compiler thing. The code never runs here.
+}
 };
