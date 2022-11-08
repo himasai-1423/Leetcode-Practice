4b6381da7a05fcfebbb46c346d7ab111d9586196
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-11-08 12:52:28 +0530

    Time: 4 ms (64.51%), Space: 6.2 MB (82.16%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        int end=0;
+        for(int cur=0; cur<s.size(); cur++){
+            if(end>0 && abs(s[cur]-s[end-1]) == 32)
+                end--;
+            
+            else{
+                s[end] = s[cur];
+                end++;
+            }
+        }
+        return s.substr(0,end);
+    }
+};
