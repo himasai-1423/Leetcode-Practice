e296571661458347d56cd3df77c12a30e9702646
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2022-12-08 22:39:10 +0530

    Time: 3 ms (63.09%), Space: 8.5 MB (13.93%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -12,18 +12,21 @@
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> res;
         stack<TreeNode*> st;
-
+        vector<int> res; 
+        
         while(root || !st.empty()){
             if(root){
                 res.push_back(root->val);
-                if(root->right) st.push(root->right);
-               root=root->left;
+                
+                if(root->right)
+                    st.push(root->right);
+
+                    root = root->left;
             }
             
             else{
-                root=st.top();
+                root = st.top();
                 st.pop();
             }
         }
