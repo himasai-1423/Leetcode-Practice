a880c5902609ae6142b58cd77c63b3df639dc8cb
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-02 15:43:04 +0530

    Time: 59 ms (36.31%), Space: 243.6 MB (5.02%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -5,10 +5,18 @@ public:
         if(idx<0) return 0;
         
         if(dp[idx][amount] != -1) return dp[idx][amount];
+        
         int ways = 0;
-        for(int coin_amt = 0; coin_amt<=amount ; coin_amt=coin_amt+coins[idx]){
-            ways += countCombinations(amount-coin_amt, coins, idx-1, dp);
+        
+        if(coins[idx] > amount)
+        {
+            ways = countCombinations(amount, coins, idx-1, dp);
         }
+        
+        else{
+            ways = countCombinations(amount-coins[idx], coins, idx, dp) + 
+                countCombinations(amount, coins, idx-1, dp);
+        }        
         
         return dp[idx][amount]= ways;
     }
