a032ef1022ef8461887d5e1bac8fb27c1b41c0e4
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-04 11:25:16 +0530

    Time: 347 ms (54.09%), Space: 401.5 MB (5.14%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     bool findPartition(vector<int> &nums, int idx, int sum, vector<vector<int>> &dp){
-        if(idx<0) return false;
+        if(idx<0 || sum<0) return false;
         if(sum == 0) return true;
         
-        if(dp[idx][sum] != -1) return dp[idx][sum];
+        if(dp[idx][sum] != -1) return dp[idx][sum];     
         
-        bool parti = findPartition(nums, idx-1, sum, dp);
-        
-        if(sum-nums[idx]>=0)
-        parti |= findPartition(nums, idx-1, sum-nums[idx], dp);
-        
-        return dp[idx][sum] = parti;
+        return dp[idx][sum] = findPartition(nums, idx-1, sum, dp) 
+            || findPartition(nums, idx-1, sum-nums[idx], dp);;
     }
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n+5, vector<int>((100*n)+5, -1));
-        int sum =0; 
+        vector<vector<int>> dp(nums.size()+5, vector<int>((100*nums.size())+5, -1));
+        int sum = accumulate(nums.begin(), nums.end(), 0); 
         
-        for(int i=0; i<n; i++){
-            sum += nums[i];
-        }
         if(sum%2 != 0) return false;
         
-        return findPartition(nums, n-1, sum/2, dp);
+        return findPartition(nums, nums.size()-1, sum/2, dp);
     }
 };
