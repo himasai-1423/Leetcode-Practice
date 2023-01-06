7601d55c12e8788a2464a9dd1f2d0fcfb8857c76
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-06 09:52:55 +0530

    Time: 73 ms (60.22%), Space: 48.5 MB (48.17%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
-    vector<int> memo;
-    bool checkCanJump(vector<int>& nums, int idx){
-        if(idx==nums.size()-1)
-            return true;
-        
-        if(memo[idx] != -1) return memo[idx];
-
-        int reachable=idx+nums[idx];
-        
-        for(int k=idx+1;k<=reachable;k++)
-            if(checkCanJump(nums, k))
-                return memo[idx] = true;
-
-        return memo[idx] = false;
-    }
-    
     bool canJump(vector<int>& nums) {
-        memo.assign(nums.size(), -1);
-        
-        return checkCanJump(nums, 0);
+        int dis = 0;
+        for (int i = 0; i <= dis; i++) {
+            dis = max(dis, i + nums[i]);
+            if (dis >= nums.size()-1) {
+                return true;
+            }
+        }
+    return false;
     }
 };
