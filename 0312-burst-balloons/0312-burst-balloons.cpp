11556808f9ade38ec83c7f360bfec40065ebc14c
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-08 21:34:14 +0530

    Time: 817 ms (31.32%), Space: 10.4 MB (20.96%) - LeetHub

---
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        vector<vector<int>> dp(nums.size()+2, vector<int>(nums.size()+2, -1));
+        vector<int> arr;
+        
+        arr.push_back(1);
+        for(int i=0; i<nums.size(); i++)
+            arr.push_back(nums[i]);
+        arr.push_back(1);
+        
+        return dfs(arr, 1, arr.size()-2, dp);        
+    }
+    
+    int dfs(vector<int> &arr, int l, int r, vector<vector<int>> &dp){
+        if(l>r) return 0;
+        
+        if(dp[l][r] != -1) return dp[l][r];
+        
+        int coins = 0; 
+        for(int i=l; i<=r; i++){
+            coins = arr[l-1]*arr[i]*arr[r+1];
+            coins += dfs(arr, i+1, r, dp) + dfs(arr, l, i-1, dp);
+            dp[l][r] = max(dp[l][r], coins);
+        }
+        return dp[l][r];
+    }
+};
