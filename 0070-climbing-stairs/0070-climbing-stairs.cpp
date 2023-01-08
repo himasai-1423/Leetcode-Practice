41bf137fad52bd2aff3aa8f9d211888d052faee0
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-08 13:23:11 +0530

    Time: 4 ms (18.29%), Space: 6.2 MB (29.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,12 @@
 class Solution {
-    public:
-    int totalStairs(int n, vector<int> &dp){
-        if(n==0) return 1;
-        else if(n<0) return 0;
-        
-        if(dp[n] != -1) return dp[n];
-        
-        return dp[n] = totalStairs(n-1, dp) + totalStairs(n-2, dp);
-    }
+public:
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-        
-        return totalStairs(n, dp);
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+        dp[1] = 2;
+        for(int i=2; i<n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n-1];
     }
 };
