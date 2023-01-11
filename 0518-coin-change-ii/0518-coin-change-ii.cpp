4a17c6791a7945d349a4179c0755da686ccdd575
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-11 18:43:02 +0530

    Time: 45 ms (47.50%), Space: 18.9 MB (22.60%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    int countCombinations(int amount, vector<int> &coins, int idx, vector<vector<int>> &dp){
-        if(amount == 0) return 1;
-        if(idx<0) return 0;
+    int coinWays(int amount, vector<int>& coins, int idx, vector<vector<int>> &dp){
+        if(amount==0) return 1;
+        
+        else if(amount<0 || idx>=coins.size())
+            return 0;
         
         if(dp[idx][amount] != -1) return dp[idx][amount];
         
         int ways = 0;
-        
-        if(coins[idx] > amount)
-        {
-            ways = countCombinations(amount, coins, idx-1, dp);
-        }
+        if(amount-coins[idx]<0)
+            ways = coinWays(amount, coins, idx+1, dp);
         
         else{
-            ways = countCombinations(amount-coins[idx], coins, idx, dp) + 
-                countCombinations(amount, coins, idx-1, dp);
-        }        
+            int takeCoin = coinWays(amount-coins[idx], coins, idx, dp);
+            int leaveCoin = coinWays(amount, coins, idx+1, dp);
+            
+            ways = takeCoin + leaveCoin;
+        }
         
-        return dp[idx][amount]= ways;
+        return dp[idx][amount] = ways;
     }
+    
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size()+3, vector<int>(amount+10, -1));
+        vector<vector<int>> dp(coins.size(), vector<int>(amount+4, -1));
+        int ways = coinWays(amount, coins, 0, dp);
         
-        return countCombinations(amount, coins, coins.size()-1, dp);
+        return ways;
     }
 };
