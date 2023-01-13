14f5485e560f2f1343df34676e8ab7c0398bfdbd
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-13 13:17:05 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (66.59%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int robHouse(vector<int> &nums, int idx, vector<int> &dp){
-        if(idx>=nums.size())
-            return 0;
-        
-        if(dp[idx]!=-1) return dp[idx];
-        
-        int stealHouse = nums[idx] + robHouse(nums, idx+2, dp);
-        int skipHouse = 0 + robHouse(nums, idx+1, dp);
-        
-        return dp[idx] = max(stealHouse, skipHouse);
-    }
     int rob(vector<int>& nums) {
-        if(nums.size()==1) return nums[0];
-        //if(nums.size()==2) return max(nums[1], nums[0]);
+       if (nums.size() == 0) return 0;
+       if (nums.size() == 1) return nums[0];
+       if (nums.size() == 2) return max(nums[0], nums[1]);
         
-        vector<int> dp(nums.size(), -1);
-        vector<int> dp2(nums.size(), -1);
+        vector<int> dp(nums.size()+1, -1);
+        vector<int> dp2(nums.size()+1, -1);
         
-        vector<int> nums1(nums.begin()+1, nums.end());
-        vector<int> nums2(nums.begin(), nums.end()-1);
+        //first element included
+        dp[0]=nums[0];
+        dp[1] = max(nums[0], nums[1]);
         
-        return max(robHouse(nums1, 0, dp), robHouse(nums2, 0, dp2));
+        for(int i=2; i<nums.size()-1; i++){
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
+        }
+        
+        //first element excluded
+        dp2[0]=0;
+        dp2[1] = nums[1];
+        dp2[2] = max(nums[1], nums[2]);
+        for(int i=3; i<nums.size(); i++){
+            dp2[i] = max(dp2[i-1], dp2[i-2]+nums[i]);
+        }
+        return max(dp[nums.size()-2], dp2[nums.size()-1]);
     }
 };
