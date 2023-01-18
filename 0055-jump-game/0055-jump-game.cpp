e6fe8ba15f0e77e77c0d6164a642634422d41a56
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-18 10:36:18 +0530

    Time: 1361 ms (7.60%), Space: 51.1 MB (9.57%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int goal=nums.size()-1;
+        vector<int> dp(nums.size(), -1);
+        return jumpGame(nums, 0, dp);
+    }
+    
+    bool jumpGame(vector<int> &nums, int i, vector<int> &dp){
+        if(i==nums.size()-1)
+            return true;
         
-        for(int i=nums.size()-1; i>=0; i--){
-            if(i+nums[i]>=goal)
-                goal = i;
+        if(dp[i] != -1) return dp[i];
+        
+        int goal = i+nums[i];
+        
+        for(int j = i+1; j<=goal; j++){
+            if(jumpGame(nums, j, dp))
+                return dp[i]= true;
         }
-        
-        return goal==0;
+        return dp[i] = false;
     }
 };
