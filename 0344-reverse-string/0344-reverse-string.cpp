97766d89878b31866e3b15a02805ff7c7d66a319
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-19 10:16:23 +0530

    Time: 24 ms (70.03%), Space: 23.2 MB (48.19%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     void reverseString(vector<char>& s) {
         int n = s.size();
+        
         for(int i=0; i<n/2; i++){
-            char temp = s[n-i-1];
-            s[n-i-1] = s[i];
-            s[i] = temp;
+            int temp = s[i];
+            s[i] = s[n-i-1];
+            s[n-i-1] = temp;
         }
     }
 };
