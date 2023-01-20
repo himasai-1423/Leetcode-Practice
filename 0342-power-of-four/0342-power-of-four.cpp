802cf02f115bf11914725b6e2f3628edad56017f
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-20 18:42:59 +0530

    Time: 3 ms (55.74%), Space: 6 MB (51.90%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isPowerOfFour(long n) {
-        return n && !(n & (n-1)) && !((n-1)%3);
+    bool isPowerOfFour(int num) {
+        return num > 0 && (num & (num - 1)) == 0 && (num - 1) % 3 == 0;
     }
 };
