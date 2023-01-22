872b0fdf2d9179504beac95a87b8f9652be7ceb4
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-22 22:42:41 +0530

    Time: 7 ms (33.53%), Space: 6.8 MB (30.47%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,22 +1,9 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        map<char, int> mS, mT;
-        
-        for(int i=0; i<s.size(); i++)
-            mS[s[i]]++;
-        
         for(int i=0; i<t.size(); i++)
-            mT[t[i]]++;
+            t[i+1] += t[i]-s[i];
         
-        for(auto i : mT){
-            if(mS.find(i.first) == mS.end())
-                return i.first;
-            
-            else if(mS[i.first] != i.second)
-                return i.first;
-        }
-        
-        return ' ';
+        return t[t.size()-1];
     }
 };
