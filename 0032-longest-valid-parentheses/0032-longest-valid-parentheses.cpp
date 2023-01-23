e7c759e0c502bc1585233ea2c31e3967fdfe7df9
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-23 11:02:58 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (47.24%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int res=0;
         stack<int> st;
+        int res = 0;
         st.push(-1);
         
         for(int i=0; i<s.size(); i++){
