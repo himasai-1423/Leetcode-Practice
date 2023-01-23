d037204b248ba2bffcb287421bd8227045584e78
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-23 22:37:26 +0530

    Time: 8 ms (20.54%), Space: 10.7 MB (44.93%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    
+    void generate(vector<int> &nums, int i, vector<int> &subarray){
+        if(i==nums.size()){
+            res.push_back(subarray);
+            return;
+        }
+        
+        generate(nums, i+1, subarray);
+        
+        subarray.push_back(nums[i]);
+        generate(nums, i+1, subarray);
+        subarray.pop_back();
+    }
+    
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> subarray;
+        generate(nums, 0, subarray);
+        return res;
+    }
+};
