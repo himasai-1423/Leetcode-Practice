2d262b583c77369c47e845a0097f43294bbd7ba9
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-24 11:28:38 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (77.20%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        long l = 1, r = num;
-        
-        while(r>=l){
-            long mid = l + ((r-l)>>1);
-            if(mid*mid == num)
-                return true;
-            
-            else if(mid>num/mid)
-                r = mid-1;
-            
-            else
-                l = mid+1;
-        } 
-        return false;
+        int sq =sqrt(num);
+        // cout<<sq<<" " <<sqrt(num)<<endl;
+        return (num == sq*sq);
     }
 };
