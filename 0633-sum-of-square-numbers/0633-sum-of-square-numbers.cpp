314d54274e1a7ecc16847ba779e4a0a5eecaf69a
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-24 11:48:26 +0530

    Time: 65 ms (8.74%), Space: 5.9 MB (53.02%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long num;
+        
+        // if(c==0 || c==1) return true;
+        
+        for(long i=0; i*i<=c; i++){
+            num = c-i*i;
+            long hi = num, lo=0;
+            while(hi>=lo){
+                long mid = lo+(hi-lo)/2;
+            
+                if(mid*mid==num) return true;
+                else if(mid*mid>num) hi = mid-1;
+                else lo = mid+1;
+            }        
+        }
+        return false;
+    }
+};
