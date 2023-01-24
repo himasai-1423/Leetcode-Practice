c4d6555197644c03175149ced4584418f3032585
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-24 23:00:45 +0530

    Time: 4 ms (28.59%), Space: 5.8 MB (89.54%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     int addDigits(int num) {
-        int res=0;
+        if(num == 0) return 0;
         
-        while(num>0){
-            res += num%10;
-            num = num/10;
-            if(num==0 && res>9){
-                num=res;
-                res=0;
-            }
-        }
-        return res;
+        if(num%9 == 0) return 9;
+        return num%9;
     }
 };
