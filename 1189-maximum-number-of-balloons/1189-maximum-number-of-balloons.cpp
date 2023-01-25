fc344077b98a85c15f0cfe5bf6aeb625906e6f1a
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-25 10:40:20 +0530

    Time: 3 ms (79.62%), Space: 6.7 MB (12.15%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        unordered_map<char, int> m;
+        for(int i=0; i<text.size(); i++){
+            m[text[i]]++;
+        }
+        int res = 0;
+        while(true){
+            if(m['b']>=1 && m['a']>=1 && m['l']>=2 && m['o']>=2 && m['n']>=1){
+                res++;
+                m['b']--;
+                m['a']--;
+                m['l']-=2;
+                m['o']-=2;
+                m['n']--;
+            }
+            
+            else
+                break;
+        }
+        return res;
+    }
+};
