a49f66eeda31ca33adef83b6d79628b9811c9a23
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-25 10:22:14 +0530

    Time: 22 ms (42.20%), Space: 13.1 MB (84.96%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        sort(begin(nums), end(nums));
+        sort(nums.begin(), nums.end());
         
         for(int i=1; i<nums.size(); i+=2){
-            if(nums[i-1] != nums[i])
+            if(nums[i]!=nums[i-1])
                 return false;
         }
-        
         return true;
     }
 };
