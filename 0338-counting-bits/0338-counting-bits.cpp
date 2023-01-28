786b28661a885517cc2c1c8206178f8c1251bd11
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-28 21:44:32 +0530

    Time: 9 ms (47.26%), Space: 8.6 MB (23.36%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -4,7 +4,11 @@ public:
         vector<int> res;
         
         for(int i=0; i<=n; i++){
-            int a = __builtin_popcount(i);
+            int a=0, j=31;
+            while(i!=0 && j>=0){
+                if(((i>>j)&1)==1) a++;
+                j--;
+            }
             res.push_back(a);
         }
         return res;
