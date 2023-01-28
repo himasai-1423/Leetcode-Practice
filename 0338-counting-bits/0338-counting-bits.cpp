463bfde7ce1bc1b230c0fa1d2e8e225eabdd0dd3
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-01-28 21:47:52 +0530

    Time: 6 ms (71.48%), Space: 7.9 MB (78.20%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> res;
+        vector<int> dp(n+1, 0);
         
-        for(int i=0; i<=n; i++){
-            int a=0, j=31;
-            while(i!=0 && j>=0){
-                if(((i>>j)&1)==1) a++;
-                j--;
-            }
-            res.push_back(a);
+        int offset =1;
+        
+        for(int i=1; i<=n; i++){
+            if(offset*2==i)
+                offset *= 2;
+            
+            dp[i] = 1+dp[i-offset];
         }
-        return res;
+        return dp;
     }
 };
