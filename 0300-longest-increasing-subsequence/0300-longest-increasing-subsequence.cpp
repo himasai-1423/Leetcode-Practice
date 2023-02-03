dbc75297cd74b2cf42ea8947a644d72cbd84dc1b
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-02-03 17:56:12 +0530

    Time: 10 ms (90.25%), Space: 10.6 MB (55.53%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    int findLIS(vector<int> &nums, int idx, vector<int> &dp){
-        int ans =1;
-        
-        if(dp[idx]!=-1) return dp[idx];
-        for(int i=0; i<idx; i++)
-            if(nums[i]<nums[idx])
-                ans = max(ans, findLIS(nums, i, dp)+1);
-        
-        return dp[idx]=ans;
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        int res = 0;
-        
+        vector<int> dp(nums.size(), 1);
+        vector<int> res;
+
         for(int i=0; i<nums.size(); i++){
-            res = max(res, findLIS(nums, i, dp));
+            auto it = lower_bound(res.begin(), res.end(), nums[i]);
+            
+            if(it==res.end())
+                res.push_back(nums[i]);
+            
+            else
+                *it = nums[i];
         }
-        return res;
+        
+        return res.size();
     }
 };
