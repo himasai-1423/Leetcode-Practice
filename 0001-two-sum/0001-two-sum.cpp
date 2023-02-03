4195bd44265f54e219e0e229185d90cdf655b331
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-02-03 19:43:17 +0530

    Time: 12 ms (85.14%), Space: 10.9 MB (29.19%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        
+        for(int i=0; i<nums.size(); i++){
+            int val = target - nums[i];
+            if(m.find(val)!=m.end()){
+                return {i, m[val]};
+            }
+            
+            m[nums[i]]=i;
+        }
+        return {};
+    }
+};
