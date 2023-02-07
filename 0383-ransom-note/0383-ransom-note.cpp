6147cf0539b7df75cfd8f4f5b01eb1ee3ba95105
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-02-07 19:20:07 +0530

    Time: 27 ms (44.30%), Space: 8.8 MB (77.16%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -3,19 +3,13 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char, int> m1;
         
+        for(int i=0; i<magazine.size(); i++)
+            m1[magazine[i]]++;
+        
         for(int i=0; i<ransomNote.size(); i++)
-            m1[ransomNote[i]]++;
+            if(!m1[ransomNote[i]]--)
+                return false;
         
-        for(int i=0; i<magazine.size(); i++){
-            if(m1.count(magazine[i]))
-                m1[magazine[i]]--;
-            
-            if(m1[magazine[i]]==0)
-                m1.erase(magazine[i]);
-        }
-        if(m1.empty())
-            return true;
-        
-        return false;
+        return true;
     }
 };
