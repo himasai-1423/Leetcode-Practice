11e7a1eeff98a5ee4016cc160adedfc9c59dc205
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-02-12 20:01:07 +0530

    Time: 216 ms (74.01%), Space: 73.9 MB (95.85%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        vector<pair<int,int>> v(speed.size(), {0, 0});
-
-        for(int i=0; i<speed.size(); i++){
-            v[i].first = position[i];
-            v[i].second = speed[i];
+        vector<pair<int, int>> p(position.size(), {0, 0});
+        
+        for(int i=0; i<position.size(); i++){
+            p[i].first = position[i];
+            p[i].second = speed[i];
         }
         
-        sort(v.begin(), v.end(),
-             [](const pair<int,int> &a, const pair<int,int> &b) { 
-                    return (a.first > b.first);
-            });
+        sort(p.begin(), p.end(), []
+             (pair<int, int> &a, pair<int, int> &b){
+                 return (a.first>b.first);
+             }
+            );
         
-        double time;
         stack<double> st;
-        for(int i=0; i<speed.size(); i++){
-            time = (double)(target-v[i].first)/v[i].second;
-            double prevTime;
+        double topPrev;
+        for(int i=0; i<position.size(); i++){
             if(!st.empty())
-                prevTime = st.top();
+                topPrev=st.top();
+            double time = (double)(target-p[i].first)/p[i].second;
             st.push(time);
-            if(st.size()>=2 && st.top()<=prevTime){
+            
+            if(st.size()>=2 && st.top()<=topPrev)
                 st.pop();
-            }
         }
         return st.size();
     }
