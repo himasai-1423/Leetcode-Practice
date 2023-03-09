a1fc042cc18c6a840ef66e006d2020bfb214035b
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-03-09 18:25:34 +0530

    Time: 10 ms (64.93%), Space: 8.8 MB (75.90%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    void swapPermute(vector<int> nums, int idx) {
+    void swapPermute(vector<int>& nums, int idx) {
         if(idx == nums.size()) {
             res.push_back(nums);
             return;
         }
         
         for(int i=idx; i<nums.size(); i++) {
-            if(idx<i && nums[idx]==nums[i]) continue;
+            if(idx!=i && nums[idx]==nums[i]) continue;
             swap(nums[i], nums[idx]);
             swapPermute(nums, idx+1);
+        }
+        for (int i = nums.size() - 1; i > idx; --i) {
+                swap(nums[idx], nums[i]);
         }
     }
     
