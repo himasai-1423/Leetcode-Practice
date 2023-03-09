d659095321bd0de0ff8a3c2368a25fb424412e00
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-03-09 18:24:31 +0530

    Time: 11 ms (62.12%), Space: 9.8 MB (37.48%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    set<vector<int>> ans;
-    void swapPermute(vector<int>& nums, int idx) {
+    vector<vector<int>> res;
+    void swapPermute(vector<int> nums, int idx) {
         if(idx == nums.size()) {
-            ans.insert(nums);
+            res.push_back(nums);
             return;
         }
         
         for(int i=idx; i<nums.size(); i++) {
+            if(idx<i && nums[idx]==nums[i]) continue;
             swap(nums[i], nums[idx]);
             swapPermute(nums, idx+1);
-            swap(nums[i], nums[idx]);
         }
     }
     
     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         swapPermute(nums, 0);
-        vector<vector<int>> res;
-        
-        for(auto i: ans) 
-            res.push_back(i);
         
         return res;        
     }
