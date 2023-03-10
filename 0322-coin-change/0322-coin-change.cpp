492f2f3fd0f2939da9f300af1b5cc49c15fa3c15
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-03-10 11:33:34 +0530

    Time: 128 ms (39.77%), Space: 45.9 MB (18.44%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,30 +1,31 @@
 class Solution {
-public:
-    int minCoins(vector<int>& coins, int amount, int idx,vector<vector<int>> &dp){
-        if(amount==0) 
-            return 0;
+public:    
+    int change(vector<int> &coins,int idx, int amt, vector<vector<int>> &dp){
+        if(amt == 0) return 0;
+        else if(idx<0 || amt<0) return INT_MAX-1;
         
-        if(amount<0 || idx>=coins.size())
-            return INT_MAX-1;
-        
-        if(dp[idx][amount] != -1) return dp[idx][amount];
-        
+        if(dp[idx][amt]!= -1) return dp[idx][amt];
+                
         int res = -1;
-        if(amount - coins[idx]<0)
-            res =  0 + minCoins(coins, amount, idx+1, dp);
+        // if(amt-coins[idx]<0){
+        //     int doNotTakeCoin = 0 + change(coins, idx-1, amt - 0);
+        //     res = doNotTakeCoin;
+        // }
         
-        else{
-            int takeCoin = 1 + minCoins(coins, amount - coins[idx], idx, dp);
-            int leaveCoin = 0 + minCoins(coins, amount, idx+1, dp);
+        // else{
+            int takeCoin = 1 + change(coins, idx, amt - coins[idx], dp);
+            int doNotTakeCoin = 0 + change(coins, idx - 1, amt - 0, dp);
             
-            res = min(takeCoin, leaveCoin);
-        }
-        return dp[idx][amount] = res;
+            res = min(takeCoin, doNotTakeCoin);
+        // }
+        
+        return dp[idx][amt] = res;
     }
     
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(), vector<int>(amount+3, -1));
-        int res = minCoins(coins, amount, 0, dp);
+        vector<vector<int>> dp(coins.size(), vector<int>(amount+1, -1));
+        int res = change(coins, coins.size()-1, amount, dp);
+        
         return res==INT_MAX-1?-1:res;
     }
 };
