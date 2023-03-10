addf8193ab5db7af68cea00ddf6ca6bde19403f3
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-03-10 11:40:02 +0530

    Time: 107 ms (57.69%), Space: 45.9 MB (18.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:    
-    int change(vector<int> &coins,int idx, int amt, vector<vector<int>> &dp){
-        if(amt == 0) return 0;
-        else if(idx<0 || amt<0) return INT_MAX-1;
+    int countCoins(vector<int> &coins, int idx, int amount, vector<vector<int>> &dp) {
+        if(amount == 0) 
+            return 0;
         
-        if(dp[idx][amt]!= -1) return dp[idx][amt];
-                
-        int res = -1;
+        if(amount<0 || idx<0) return INT_MAX-1;
         
-        int takeCoin = 1 + change(coins, idx, amt - coins[idx], dp);
-        int doNotTakeCoin = 0 + change(coins, idx - 1, amt - 0, dp);
+        if(dp[idx][amount] != -1) return dp[idx][amount];
+        
+        int ans=-1;
+        
+        int takeCoin = countCoins(coins, idx, amount-coins[idx], dp)+1;
+        int leaveCoin = countCoins(coins, idx-1, amount, dp);
             
-        res = min(takeCoin, doNotTakeCoin);
+        ans = min(takeCoin, leaveCoin);
         
-        return dp[idx][amt] = res;
+                
+        return dp[idx][amount]=ans;
     }
     
     int coinChange(vector<int>& coins, int amount) {
         vector<vector<int>> dp(coins.size(), vector<int>(amount+1, -1));
-        int res = change(coins, coins.size()-1, amount, dp);
+        int res = countCoins(coins, coins.size()-1, amount, dp);
         
         return res==INT_MAX-1?-1:res;
     }
