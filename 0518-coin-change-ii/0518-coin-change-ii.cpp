e3c08f6ea05be561e89a2b383d4938b260b2e199
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-03-11 15:34:35 +0530

    Time: 55 ms (26.21%), Space: 18.6 MB (42.31%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-    int coinWays(int amount, vector<int>& coins, int idx, vector<vector<int>> &dp){
-        if(amount==0) return 1;
-        
-        else if(amount<0 || idx>=coins.size())
-            return 0;
-        
-        if(dp[idx][amount] != -1) return dp[idx][amount];
-        
-        int ways = 0;
-        if(amount-coins[idx]<0)
-            ways = coinWays(amount, coins, idx+1, dp);
-        
-        else{
-            int takeCoin = coinWays(amount-coins[idx], coins, idx, dp);
-            int leaveCoin = coinWays(amount, coins, idx+1, dp);
-            
-            ways = takeCoin + leaveCoin;
-        }
-        
-        return dp[idx][amount] = ways;
-    }
-    
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size(), vector<int>(amount+4, -1));
-        int ways = coinWays(amount, coins, 0, dp);
+        vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1, 0));
         
-        return ways;
+        for(int i=1; i<=coins.size(); i++) 
+            dp[i][0] = 1;
+        
+        for(int i=1; i<=coins.size(); i++) {
+            for(int j=1; j<=amount; j++) {
+                if(coins[i-1]>j)
+                    dp[i][j] = dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+            }
+        }
+        return dp[coins.size()][amount];
     }
 };
