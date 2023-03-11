310243c7e3d4c62329dad209b07ac9c0654708aa
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-03-11 15:35:17 +0530

    Time: 60 ms (20.39%), Space: 18.5 MB (42.31%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -3,7 +3,7 @@ public:
     int change(int amount, vector<int>& coins) {
         vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1, 0));
         
-        for(int i=1; i<=coins.size(); i++) 
+        for(int i=0; i<=coins.size(); i++) 
             dp[i][0] = 1;
         
         for(int i=1; i<=coins.size(); i++) {
