34a77a87e33d1ccd4e5b9410d79ca7cc0890f71b
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-03-12 12:35:21 +0530

    Time: 250 ms (17.99%), Space: 13.3 MB (58.02%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0)
+            return NULL;
+        
+        while(lists.size()>1) {
+            lists.push_back(mergeLists(lists[0], lists[1]));
+            lists.erase(lists.begin());
+            lists.erase(lists.begin());
+        }
+        return lists.front();
+    }
+    
+    ListNode* mergeLists(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* temp = dummy;
+        
+        while(l1 && l2) {
+            if(l1->val<=l2->val) {
+                temp->next = l1;
+                l1 = l1->next;
+            }
+            
+            else {
+                temp->next = l2;
+                l2 = l2->next;
+            }
+            temp = temp->next;                
+        }
+        if(l1)
+            temp->next = l1;
+        
+        else if(l2)
+            temp->next = l2;
+        
+        return dummy->next;
+    }
+};
