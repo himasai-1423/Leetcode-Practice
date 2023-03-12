d306c7b0e8f014f22bda52b49c2df5b34360b97f
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-03-12 12:37:41 +0530

    Time: 250 ms (17.99%), Space: 13.3 MB (58.02%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -23,6 +23,9 @@ public:
     }
     
     ListNode* mergeLists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        
         ListNode* dummy = new ListNode(0);
         ListNode* temp = dummy;
         
@@ -30,19 +33,14 @@ public:
             if(l1->val<=l2->val) {
                 temp->next = l1;
                 l1 = l1->next;
-            }
-            
-            else {
+            } else {
                 temp->next = l2;
                 l2 = l2->next;
             }
             temp = temp->next;                
         }
-        if(l1)
-            temp->next = l1;
-        
-        else if(l2)
-            temp->next = l2;
+        if(l1) temp->next = l1;
+        else if(l2) temp->next = l2;
         
         return dummy->next;
     }
