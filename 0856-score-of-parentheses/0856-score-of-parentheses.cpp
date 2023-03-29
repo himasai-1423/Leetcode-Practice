33408ceb9ff2ba19851d01d8cb1ae82cc553d99a
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-03-29 19:52:48 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (52.49%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.cpp
+++ 0856-score-of-parentheses/0856-score-of-parentheses.cpp
@@ -3,9 +3,10 @@ public:
     int scoreOfParentheses(string s) {
         stack<int> st;
         
-        int score = 0;        
+        int score = 0;
+        
         for(int i=0; i<s.size(); i++) {
-            if(s[i]=='(') {
+            if(s[i]=='('){
                 st.push(score);
                 score=0;
             }
