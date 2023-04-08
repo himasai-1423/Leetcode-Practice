1a9793b7a45005320738cfa7decf337a61a6cc3c
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-04-08 11:29:46 +0530

    Time: 288 ms (66.98%), Space: 10.7 MB (34.94%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,18 +1,25 @@
 class Solution {
-public:
+public:    
+    int findLen(vector<int>& nums, vector<int>& dp, int idx) {
+        if(idx<0) return 0;
+        
+        if(dp[idx]!=-1) return dp[idx];
+        
+        int ans = 1;
+        for(int i = idx; i>=0; i--) 
+            if(nums[i]<nums[idx]) 
+                ans = max(ans, 1 + findLen(nums, dp, i));
+            
+        return dp[idx]=ans;
+    }
+    
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> res;
-
-        for(int i=0; i<nums.size(); i++){
-            auto it = lower_bound(res.begin(), res.end(), nums[i]);
-            
-            if(it==res.end())
-                res.push_back(nums[i]);
-            
-            else
-                *it = nums[i];
+        vector<int> dp(nums.size(), -1);
+        int res = 1;
+        for(int i = nums.size()-1; i>=0; i--) {
+            res = max(res, findLen(nums, dp, i));
         }
         
-        return res.size();
+        return res;
     }
 };
