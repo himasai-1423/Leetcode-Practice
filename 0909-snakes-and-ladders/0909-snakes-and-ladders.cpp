c868e26b8652c39ad47082a83c2daf99b74f3aac
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-04-30 16:23:10 +0530

    Time: 29 ms (34.91%), Space: 13.9 MB (29.72%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-    pair<int, int> intToPos(int square, int n) {
-        int r = (square-1)/n;
-        int c = (square-1)%n;
+    pair<int, int> intToDim(int cell, int n) {
+        int r = (cell-1)/n;
+        int c = (cell-1)%n;
         if(r%2)
             c = n - 1 - c;
         return {r, c};
     }
-    
     int snakesAndLadders(vector<vector<int>>& board) {
-        int n = board.size();
+        int  n=board.size();
         reverse(board.begin(), board.end());
         
-        set<int> vis;
+        unordered_set<int> vis; 
         queue<pair<int, int>> q;
-        q.push({1, 0}); //{square, moves}
+        q.push({1, 0}); //{cell, moves}
+        
         while(!q.empty()) {
-            auto node = q.front();
-            int square = node.first;
-            int moves = node.second;
+            auto cell = q.front();
+            int square = cell.first;
+            int moves = cell.second;
             q.pop();
-            for(int i = 1; i<7; i++) {
-                int nextSquare = i + square;
-                auto dim = intToPos(nextSquare, n);
-                if(board[dim.first][dim.second] != -1) 
-                    nextSquare = board[dim.first][dim.second];
-                if(nextSquare == n*n)
-                    return moves + 1;
+            
+            for(int i=1; i<=6; i++) {
+                int nextCell = i + square;              
+                auto dim = intToDim(nextCell, n);
+                int x = dim.first;
+                int y = dim.second;
                 
-                if(!vis.count(nextSquare)) {
-                    vis.insert(nextSquare);
-                    q.push({nextSquare, moves + 1});
-                } 
+                if(board[x][y]!=-1) nextCell = board[x][y];
+                if(nextCell==n*n) return moves+1;
+                
+                if(!vis.count(nextCell)) {
+                    vis.insert(nextCell);
+                    q.push({nextCell, moves+1});
+                }   
             }
         }
         return -1;
