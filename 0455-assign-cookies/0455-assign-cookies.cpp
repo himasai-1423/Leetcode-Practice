ca883e3d602e81de3df1fe2e2d12a0b64bfcad27
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-05-01 21:03:35 +0530

    Time: 31 ms (65.24%), Space: 17.4 MB (83.11%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int res = 0, i = 0, j = 0;
+        
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        
+        while(i<g.size() && j<s.size()) {
+            if(g[i]<=s[j]) {
+                res++;
+                i++;
+                j++;
+            }
+            else
+                j++;
+        }
+        return res;
+    }
+};
