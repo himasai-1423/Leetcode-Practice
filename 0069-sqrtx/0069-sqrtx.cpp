48c341455a2a1caddd13acc46be9b39b901b3070
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-05-02 21:28:38 +0530

    Time: 7 ms (32.39%), Space: 5.9 MB (61.71%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0||x==1) return x;
+        
+        int last = x, first = 0, ans;
+
+        while(last>first){
+            int mid = first + (last - first)/2;
+            if(mid == (x/mid)) return mid;
+            
+            else if(mid<=(x/mid)){
+                first = mid + 1;
+                ans = mid;
+            }             
+            else
+                last = mid;
+        }
+        return ans;
+    }
+};
