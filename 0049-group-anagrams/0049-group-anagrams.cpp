aa7e01dff32a3ff24564b110a4755356da77a022
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-05-05 23:58:25 +0530

    Time: 31 ms (88.81%), Space: 19.7 MB (81.21%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> m;
+        
+        for(auto &s: strs) {
+            string t = s;
+            sort(t.begin(), t.end());
+            m[t].push_back(s);
+        }
+        
+        vector<vector<string>> res;
+        for(auto &a: m) 
+            res.push_back(a.second);
+        
+        return res;
+    }
+};
