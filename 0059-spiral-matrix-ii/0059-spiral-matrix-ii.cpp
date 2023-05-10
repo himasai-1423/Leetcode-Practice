b95fb1065faa274725354978d33b8adccef151ff
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-05-10 23:01:08 +0530

    Time: 5 ms (6.36%), Space: 6.7 MB (23.20%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -2,49 +2,49 @@ class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
         vector<vector<int>> res(n, vector<int>(n, 0));
-        int num = 1;
-        int dir = 1, right = n-1, left = 0, up = 0, down = n-1;
+        int dir = 1, num = 1;
+        int left = 0, right = n-1, top = 0, down = n-1;
         
-       while(left<=right && up<=down) {
-           if(dir == 1) {
-               int i = left;
-               while(i<=right) {
-                   res[up][i]=num++;
-                   i++;
-               }
-               up++;
-               dir = 2;
-           }
-           
-           else if(dir == 2) {
-               int i = up;
-               while(i<=down) {
-                   res[i][right]=num++;
-                   i++;
-               }
-               right--;
-               dir = 3;
-           }
-           
-           else if(dir == 3) {
-               int i = right;
-               while(i>=left) {
-                   res[down][i]=num++;;
-                   i--;
-               }
-               down--;
-               dir = 4;
-           }
-           
-           else {
-               int i = down;
-               while(i>=up) {
-                   res[i][left]=num++;
-                   i--;
-               }
-               left++;
-               dir = 1;
-           }
+        while(left<=right && top<=down) {
+            if(dir==1) {
+                int i = left;
+                while(i<=right) {
+                    res[top][i] = num++;
+                    i++;
+                }
+                dir = 2;
+                top++;
+            }
+            
+            else if(dir==2) {
+                int i = top;
+                while(i<=down) {
+                    res[i][right] = num++;
+                    i++;
+                }
+                dir = 3;
+                right--;
+            }
+            
+            else if(dir==3) {
+                int i = right;
+                while(i>=left) {
+                    res[down][i] = num++;
+                    i--;
+                }
+                dir = 4;
+                down--;
+            }
+            
+            else {
+                int i = down;
+                while(i>=top) {
+                    res[i][left] = num++;
+                    i--;
+                }
+                dir = 1;
+                left++;
+            }
         }
         return res;
     }
