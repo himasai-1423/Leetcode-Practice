54dce4ccd525fd77b137872410d2f97866104ab6
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-05-10 22:52:41 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (23.20%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -1,25 +1,51 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> result (n, vector<int>(n));
-        int cnt = 1;
-        for (int layer = 0; layer < (n + 1) / 2; layer++) {
-            for (int ptr = layer; ptr < n - layer; ptr++) {
-                result[layer][ptr] = cnt++;
-            }
-
-            for (int ptr = layer + 1; ptr < n - layer; ptr++) {
-                result[ptr][n - layer - 1] = cnt++;
-            }
-
-            for (int ptr = n - layer - 2; ptr >= layer; ptr--) {
-                result[n - layer - 1][ptr] = cnt++;
-            }
-
-            for (int ptr = n - layer - 2; ptr > layer; ptr--) {
-                result[ptr][layer] = cnt++;
-            }
+        vector<vector<int>> res(n, vector<int>(n, 0));
+        int num = 1;
+        int dir = 1, right = n-1, left = 0, up = 0, down = n-1;
+        
+       while(left<=right && up<=down) {
+           if(dir == 1) {
+               int i = left;
+               while(i<=right) {
+                   res[up][i]=num++;
+                   i++;
+               }
+               up++;
+               dir = 2;
+           }
+           
+           else if(dir == 2) {
+               int i = up;
+               while(i<=down) {
+                   res[i][right]=num++;
+                   i++;
+               }
+               right--;
+               dir = 3;
+           }
+           
+           else if(dir == 3) {
+               int i = right;
+               while(i>=left) {
+                   res[down][i]=num++;;
+                   i--;
+               }
+               down--;
+               dir = 4;
+           }
+           
+           else {
+               int i = down;
+               while(i>=up) {
+                   res[i][left]=num++;
+                   i--;
+               }
+               left++;
+               dir = 1;
+           }
         }
-        return result;
+        return res;
     }
 };
