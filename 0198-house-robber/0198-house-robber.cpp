ce0db7b10652636fa6f0af42198453e6d1654400
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-05-12 14:11:00 +0530

    Time: 2 ms (46.91%), Space: 7.7 MB (84.70%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
-    int houseRob(vector<int>& nums, int idx, vector<int> &dp){
-        if(idx>=nums.size()) return 0;
-        
-        if(dp[idx]!=-1) return dp[idx];
-        
-        int steal = nums[idx] + houseRob(nums, idx+2, dp);
-        int skip = houseRob(nums, idx+1, dp);
-        
-        return dp[idx]=max(steal, skip);
-    }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return houseRob(nums, 0, dp);
+        vector<int> dp(nums.size()+1, 0);
+        dp[1] = nums[0];
+        for(int i=1; i<nums.size(); i++) {
+            dp[i+1] = max(nums[i]+dp[i-1], dp[i]);
+        }
+        return dp[nums.size()];
     }
 };
