495bfd5861c9c9e41792d37508bf6def0789a80e
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-05-14 17:26:08 +0530

    Time: 75 ms (5.93%), Space: 41.5 MB (99.63%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -5,5 +5,11 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    return nums.reduce(fn, init);
+    let val = init;
+    
+    for(let i=0; i<nums.length; i++) {
+        val = fn(val, nums[i]);
+    }
+    
+    return val;
 };
