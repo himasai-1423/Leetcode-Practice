257d5aa5ddca39ee664cb7a5087ca8de104fb94a
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-05-14 17:04:02 +0530

    Time: 138 ms (9.40%), Space: 10.3 MB (93.07%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-        long long res=0, n= nums.size();
-        stack<int> st;
+        int n = int(nums.size());
+        long long answer = 0;
         
-        for(int i=0; i<=n; i++){
-            while(!st.empty() && (i==n || nums[st.top()]>nums[i])){
-                int mid = st.top();
-                st.pop();
-                int left = (st.empty()?-1:st.top());
-                
-                long long ans = (long long)(mid-left)*(i-mid)*nums[mid];
-                
-                res -= ans;
+        for (int left = 0; left < n; ++left) {
+            int minVal = nums[left], maxVal = nums[left];
+            for (int right = left; right < n; ++right) {
+                maxVal = max(maxVal, nums[right]);
+                minVal = min(minVal, nums[right]);
+                answer += maxVal - minVal;
             }
-            st.push(i);
         }
         
-        st = stack<int>();
-        
-        for(int i=0; i<=n; i++){
-            while(!st.empty() && (i==n || nums[st.top()]<nums[i])){
-                int mid = st.top();
-                st.pop();
-                int left = (st.empty()?-1:st.top());
-                
-                long long ans = (long long)(mid-left)*(i-mid)*nums[mid];
-                
-                res += ans;
-            }
-            st.push(i);
-        }
-        return res;
+        return answer;
     }
 };
