0f1623ef2e38346649a55cc11acb5d7765977ea2
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-05-18 19:59:46 +0530

    Time: 35 ms (35.14%), Space: 8.1 MB (28.32%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if(t=="") return "";
+        unordered_map<char, int> countT, countS;
+        
+        for(auto i: t)
+            countT[i]++;
+        
+        int resLen = INT_MAX, l = 0, have = 0, need = countT.size();
+        pair<int, int> resDim;
+        for(int r = 0; r<s.size(); r++) {
+            countS[s[r]]++;
+            
+            if(countT.count(s[r]) && countT[s[r]] == countS[s[r]])
+                have++;
+            
+            while(have == need) {
+                if(r-l+1<resLen) {
+                    resDim = {l, r};
+                    resLen = r-l+1;
+                }
+                countS[s[l]]--;
+                
+                if(countT.count(s[l]) && countS[s[l]]<countT[s[l]]) 
+                    have--;
+                                  
+                l++;
+            }
+        }
+        if(resLen==INT_MAX)
+            return "";
+        
+        return s.substr(resDim.first, resLen);
+    }
+};
