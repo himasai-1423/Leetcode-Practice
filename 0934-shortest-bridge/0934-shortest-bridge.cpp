f3b1476ee02c5dde6a1a4066fb42dc8cd9a0674c
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-05-21 13:46:03 +0530

    Time: 56 ms (54.58%), Space: 20.4 MB (62.34%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.cpp
+++ 0934-shortest-bridge/0934-shortest-bridge.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
+    vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
     queue<pair<int, int>> q;
-    vector<pair<int, int>> moves = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     
     void dfs(int i, int j, vector<vector<int>>& grid) {
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[i].size() || grid[i][j]!=1 )
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[i].size() || grid[i][j]!=1)
             return;
         
         grid[i][j] = 2;
         q.push({i, j});
-        for(auto &move: moves) {
-            int x = move.first + i;
-            int y = move.second + j;
+        for(auto &dir: directions) {
+            int x = i + dir.first;
+            int y = j + dir.second;
             dfs(x, y, grid);
         }
     }
     
     int bfs(vector<vector<int>>& grid) {
-        int dist = 0;
-        int minDist = INT_MAX;
-        
+        int dist = 0, minDist = INT_MAX;
         while(!q.empty()) {
             int n = q.size();
             while(n--) {
                 auto node = q.front();
                 q.pop();
                 
-                for(auto &move: moves) {
-                    int x = move.first + node.first;
-                    int y = move.second + node.second;
-                    if(x>=0 && y>=0 && x<grid.size() && y<grid[0].size()) {
-                        if(grid[x][y]==1){
-                            minDist = min(minDist, dist);
-                        }
+                for(auto &dir: directions) {
+                    int x = dir.first + node.first;
+                    int y = dir.second + node.second;
+                    
+                    if(x>=0 && y>=0 && x<grid.size() && y<grid[x].size()) {
+                        if(grid[x][y]==1) 
+                            minDist = min(dist, minDist);
+                        
                         else if(grid[x][y]==0){
                             q.push({x, y});
-                            grid[x][y]=-1;
+                            grid[x][y] = -1;
                         }
                     }
                 }
@@ -46,17 +45,18 @@ public:
     }
     
     int shortestBridge(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
         bool flag = false;
-        for(int i=0;i<grid.size(); i++) {
-            for(int j=0; j<grid[i].size(); j++) {
-                if(grid[i][j]==1) {
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(grid[i][j]) {
                     dfs(i, j, grid);
                     flag = true;
                     break;
-                }   
+                }
             }
             if(flag) break;
-        }                
+        }
         return bfs(grid);
     }
 };
