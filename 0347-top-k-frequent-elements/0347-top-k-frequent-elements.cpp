d8b95b17641159457ea5f9764b23750ab69ddf39
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-05-22 22:47:39 +0530

    Time: 18 ms (57.73%), Space: 14 MB (16.76%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> m;
+        unordered_map<int, int> m1;
         multimap<int, int> m2;
         vector<int> res;
         
         for(int i=0; i<nums.size(); i++) 
-            m[nums[i]]++;
+            m1[nums[i]]++;
         
-        for(auto i: m)
+        for(auto &i: m1) 
             m2.insert({i.second, i.first});
         
         for(auto it = m2.rbegin(); it!=m2.rend(); it++) {
+            k--;
+            res.push_back(it->second);
             if(k==0)
                 break;
-            res.push_back(it->second);
-            k--;
         }
         return res;
     }
