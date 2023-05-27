08e8b5c05742592afcb5c5a9089224a1ce54803f
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-05-27 13:38:47 +0530

    Time: 31 ms (26.39%), Space: 16.4 MB (21.82%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
+    int dfs(int i, int j, vector<vector<int>> &dp, vector<int> &piles) {
+        if(i>j) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int left = (j-i)%2==0?piles[i]:0;
+        int right = (j-1)%2==0?piles[j]:0;
+        
+        dp[i][j] = max(dfs(i+1, j, dp, piles) + left, dfs(i, j-1, dp, piles) + right);
+        return dp[i][j];
+    }
+    
     bool stoneGame(vector<int>& piles) {
-        priority_queue<int> pq;
-        
-        for(auto &i: piles) 
-            pq.push(i);
-        
-        int Alice = 0, Bob = 0;
-        for(int i=0; i<piles.size(); i++) {
-            if(i%2==0) 
-                Alice += pq.top();
-            else 
-                Bob += pq.top();
-            pq.pop();
-        }
-        return Alice>Bob;
+        vector<vector<int>> dp(piles.size(), vector<int>(piles.size(), -1));
+        return dfs(0, piles.size()-1, dp, piles)
+            >=accumulate(piles.begin(), piles.end(), 0)/2;
     }
 };
