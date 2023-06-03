ba091765c1502a711a16920af41ba2aab2969453
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-06-03 19:14:13 +0530

    Time: 107 ms (80.36%), Space: 14.6 MB (82.88%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    void dfs(int i, int &cnt, vector<int> adj[], vector<bool> &vis) {
+    int dfs(int i, vector<int> adj[], vector<bool> &vis) {
         vis[i] = true;
-        cnt++;
+        int cnt = 1;
         for(int &child: adj[i]) 
             if(!vis[child])
-                dfs(child, cnt, adj, vis);
+                cnt +=dfs(child, adj, vis);
+        return cnt;
     }
     
     int maximumDetonation(vector<vector<int>>& bombs) {
         int n = bombs.size();
         vector<int> adj[n];
         
-//         for(int i=0; i<n; i++) {
-//             long long x1=bombs[i][0] , y1=bombs[i][1], r1=bombs[i][2];
-//             for(int j=i+1; j<n; j++) {
-//                 long long x2 = bombs[j][0] , y2=bombs[j][1], r2=bombs[j][2];
-//                 long long x = (x1-x2)*(x1-x2);
-//                 long long y = (y1-y2)*(y1-y2);
-//                 long long d = sqrt(x+y);
-                
-//                 if(d<=r1)
-//                     adj[i].push_back(j);
-//                 if(d<=r2)
-//                     adj[j].push_back(i);
-//             }
-//         }
         for(int i=0; i<n; i++){
             long long int x0, y0, r0;
             x0 = bombs[i][0];
@@ -47,8 +34,7 @@ public:
         int res = INT_MIN;
         for(int i=0; i<n; i++) {
             vector<bool> vis(n);
-            int cnt = 0;
-            dfs(i, cnt, adj, vis);
+            int cnt = dfs(i, adj, vis);
             res = max(res, cnt);
         }
         return res;
