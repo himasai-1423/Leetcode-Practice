83802a5b682a2cdd342c6a76e84f85ee714420fe
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-06-14 10:54:42 +0530

    Time: 48 ms (23.45%), Space: 25.9 MB (88.84%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -13,22 +13,21 @@ class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int rootIndex = 0;
-        return builder(preorder, inorder, rootIndex, 0, inorder.size()-1);
+        return treeBuilder(preorder, inorder, rootIndex, 0, inorder.size()-1);
     }
     
-    TreeNode* builder(vector<int>& preorder, vector<int>& inorder, int& rootIndex, int left, int right){
-        if(left>right) return NULL;
-        
-        int pivot=left;
+    TreeNode* treeBuilder(vector<int>& preorder, vector<int>& inorder, int &rootIndex, int l, int r) {
+        if(l>r) return NULL;
+        int pivot = l;
         
         while(inorder[pivot] != preorder[rootIndex]) pivot++;
-        rootIndex++;
         
         TreeNode* node = new TreeNode(inorder[pivot]);
+        rootIndex++;
         
-        node->left = builder(preorder, inorder, rootIndex, left, pivot-1);
-        node->right = builder(preorder, inorder, rootIndex, pivot+1, right);
-        
+        node->left = treeBuilder(preorder, inorder, rootIndex, l , pivot-1);
+        node->right = treeBuilder(preorder, inorder, rootIndex, pivot+1, r);
+
         return node;
     }
 };
