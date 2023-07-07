559023f3d8fd91c2864d8c57e4577ab09f586b58
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-07-07 18:29:50 +0530

    Time: 30 ms (52.43%), Space: 11.9 MB (20.67%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int check(string answerKey, int k, char ch) {
+        int l=0, res = 0;
+        for(int i=0; i<answerKey.size(); i++) {
+            if(answerKey[i]!=ch) {
+                k--;
+                while(k<0) {
+                    if(answerKey[l]!=ch) k++;
+                    l++;
+                }
+            }
+            
+            res = max(res, i-l+1);
+        }
+        return res;
+    }
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        return max(check(answerKey, k, 'T'), check(answerKey, k, 'F'));
+    }
+};
