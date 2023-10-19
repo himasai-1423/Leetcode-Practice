3f09353e2d7b395910914daa3007df346b95261a
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-10-19 21:37:26 +0530

    Time: 3 ms (94.66%), Space: 4.3 MB (14.67%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.go
@@ -0,0 +1,10 @@
+func twoSum(nums []int, target int) []int {
+    m := make(map[int]int)
+    for i, num := range nums {
+        if idx, ok := m[target-num]; ok {
+            return []int{i, idx}
+        }
+        m[num] = i
+    }
+    return []int{-1, -1}
+}
