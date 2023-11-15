2d41047287149f70e2a0b48d238cde6cf5bd6019
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-11-15 21:41:32 +0530

    Time: 44 ms (67.77%), Space: 7.7 MB (63.15%) - LeetHub

---
+++ 0015-3sum/0015-3sum.go
@@ -0,0 +1,28 @@
+func threeSum(nums []int) [][]int {
+    var res [][]int
+    sort.Ints(nums)
+    
+    for i:=0; i<len(nums)-2; i++ {
+        if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+        
+        l:= i+1;
+        r:= len(nums)-1
+        for l<r {
+            sum := nums[i] + nums[l] + nums[r]
+            if sum == 0 {
+                res = append(res, []int{nums[i], nums[l], nums[r]})
+                l++
+                for l<r && nums[l] == nums[l-1] {
+                    l++
+                }
+            } else if sum < 0 {
+                l++
+            } else {
+                r--
+            }
+        }
+    }
+    return res
+}
