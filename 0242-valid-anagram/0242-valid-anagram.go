18d75cd9b6bd62b3af573656e1c48a62faf04ac4
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-12-16 17:59:51 +0530

    Time: 6 ms (61.60%), Space: 2.8 MB (34.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.go
+++ 0242-valid-anagram/0242-valid-anagram.go
@@ -1,23 +1,22 @@
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        unordered_map<int, int> m;
-        
-        if(s.size()!=t.size())
-            return false;
-        
-        for(int i=0; i<s.size(); i++) 
-            m[s[i]]++;
-        
-        for(int i=0; i<t.size(); i++) {
-            if(m.count(t[i])) {
-                m[t[i]]--;
-                if(m[t[i]]<0)
-                    return false;
-            } else {
-                return false;
-            }
-        }
-        return true;
+func isAnagram(s string, t string) bool {
+    m := make(map[rune]int)
+
+    if len(s) != len(t) {
+        return false
     }
-};
+
+    for _, char := range s {
+        m[char]++
+    }
+
+    for _, char := range t {
+        m[char]--
+    }
+    
+    for _, elem := range m {
+        if elem != 0 {
+            return false
+        } 
+    }
+    return true
+}
