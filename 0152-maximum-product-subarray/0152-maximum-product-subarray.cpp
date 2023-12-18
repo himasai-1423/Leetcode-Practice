00c884ca87cfff5d8ebaa68b863a98ecee5ad967
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-12-18 09:14:51 +0530

    Time: 10 ms (16.34%), Space: 14.4 MB (9.40%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int mini = 1, maxi=1, res=INT_MIN;
+        int mini = 1, maxi = 1, res = INT_MIN;
         
-        for(int i : nums){
-            if(i==0){
+        for(int i: nums) {
+            if(i==0) {
                 mini=maxi=1;
+                res=max(res, 0);
+                continue;
             }
-                
-            int temp = maxi * i;
             
-            maxi = max({temp, mini*i, i});
-            mini = min({temp, mini*i, i});
+            int temp = i*maxi;
+            maxi = max({temp, i*mini, i});
+            mini = min({temp, i*mini, i});
+            
             res = max(res, maxi);
         }
         return res;
