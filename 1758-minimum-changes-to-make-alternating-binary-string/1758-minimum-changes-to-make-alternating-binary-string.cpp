425bea354a2dfd5ab33e86797d28f4f5a10c2f71
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2023-12-24 20:34:07 +0530

    Time: 6 ms (35.61%), Space: 7.2 MB (86.65%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int minOperations(string s) {
         char flag = '1';
-        int oneCnt = 0;
+        int oneOddCnt = 0, zeroZeroCnt = 0;
         
         for(int i=0; i<s.size(); i++) {
             if(s[i] != flag) 
-                oneCnt++;
+                oneOddCnt++;
+            else
+                zeroZeroCnt++;
             
             if(flag == '1') 
                 flag = '0';
@@ -14,18 +16,6 @@ public:
                 flag = '1';
         }
         
-        flag = '0';
-        int zeroCnt = 0;
-        for(int i=0; i<s.size(); i++) {
-            if(s[i] != flag)
-                zeroCnt++;
-            
-            if(flag == '1')
-                flag = '0';
-             else 
-                flag = '1';
-        }
-        
-        return min(zeroCnt, oneCnt);
+        return min(oneOddCnt, zeroZeroCnt);
     }
 };
