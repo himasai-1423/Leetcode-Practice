3fd294b3ad2dba3536bfa5374112a82641565737
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2024-05-31 20:09:00 +0530

    Time: 13 ms (11.37%), Space: 14.3 MB (18.19%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
+        vector<int> ans;
         unordered_map<int, int> m;
-        vector<int> res;
-        
-        for(int i=0; i<nums.size(); i++){
+        for(int i=0; i<nums.size(); i++) {
             m[nums[i]]++;
         }
         
-        for(int &i: nums){
-            if(m[i]==1) res.push_back(i);
+        for(auto i: m) {
+            if(i.second == 1) {
+                ans.push_back(i.first);
+            }
         }
-        return res;
+        return ans;
     }
 };
