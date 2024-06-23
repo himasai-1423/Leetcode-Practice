ba8dd95e28a2e0d8efc9e3d09a5448e8651662eb
Himasai Reddy <106570815+himasai-1423@users.noreply.github.com>
2024-06-23 20:10:18 +0530

    Time: 1 ms (80.22%), Space: 2.2 MB (45.43%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.go
@@ -0,0 +1,10 @@
+func isSubsequence(s string, t string) bool {
+    j := 0
+    
+    for i := 0; i<len(t) && j<len(s); i++ {
+        if s[j] == t[i] {
+            j++
+        }
+    }
+    return len(s) == j
+}
